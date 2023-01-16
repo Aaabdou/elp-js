05436fda08ef117f1a47941749c3f669f1c454d9
YunweiSun <sunyunwei629@gmail.com>
2023-01-16 16:54:26 +0100

    cli
    upload le 16 jan, avec les fonctions copie, remove, touch etc.

--- CLI_interactif.js
+++ CLI_interactif.js
@@ -1,63 +1,66 @@
 var fs = require('fs');
-const inquirer = require('inquirer');
 const readline = require('readline');
-/* const options = require('options')
-    .option("dir") */
-
 const input = readline.createInterface(process.stdin, process.stdout);
-function askname(){
-    inquirer.prompt([
-        {
-        name: "doss", 
-        type: "input", 
-        message: "Nom du dossier ?", 
-        },
-    ]).then((answer) => {console.log("Hello " + answer.doss);});
-}
-async function test(){
-    input.question('Hello\n', (response) => {
-        if(response == "hello") {
-            console.log("Bonjour");
-    
-        } else if(response == "create"){
-            toto = await askname();
-            
-            fs.mkdir(answer.doss, function (error) {  
-                if (error) {  
-                    console.error('échec de création du répertoire', error);  
-                }   else {  
-                    console.log('répertoire créé');  
-                }  
-            });
-    
-        } else{
-            console.log("Error");
-        }
-    
-        process.exit();
-    });
-    
-    
+const prompt = require('prompt-sync')();
+
+const f = () => {
+    return new Promise(resolve => input.question("Hello\n", resolve));
 }
 
-input.question('Hello\n', (response) => {
-	if(response == "hello") {
-		console.log("Bonjour");
-
-	} else if(response == "create"){
-        await askname();
-        
-		fs.mkdir(answer.doss, function (error) {  
+const operation = (response) => {
+	if(response == "mkdir"){
+        const name = prompt('Nom du dossier: ');
+		fs.mkdir(name, function (error) {  
             if (error) {  
-                console.error('échec de création du répertoire', error);  
+                console.error('Echec de création du répertoire', error);  
             }   else {  
-                console.log('répertoire créé');  
+                console.log('Répertoire créé');  
             }  
         });
+	} else if(response == "dir"){
+        //new Promise(directory => input.question("Lecture du répertoire\n", directory));
+        const name = prompt('Nom du répertoire: ');
+        fs.readdir(name, function (error, files) {  
+            if (error) {  
+                console.error('échec de lecture du répertoire', error);  
+            } else {  
+                console.log('Fichiers trouvés:', files);  
+            }  
+        });
+    } else if (response == "touch"){
+        const name = prompt("Nom du fichier: ");
+        const contenu = prompt("Contenu: ");
+        fs.appendFile(name, contenu, function (error) {   
+            if (error) {
+                console.error("Erreur'", error); }   
+            else console.log("Fichier créé !");
+        });
+        
+    } else if (response == "cp"){
+        const nameFichier = prompt("Nom du fichier à copier: ");
+        const nameFichierCopie = prompt("Nom du fichier copié: ");
+        fs.copyFile(nameFichier, nameFichierCopie, function(error) { 
+            if (error) {
+                console.error("Erreur'", error); } 
+            else console.log("Fichier copié !"); 
+        });
 
-	} else{
-        console.log("Error");
+    } else if (response == "rm"){
+        const supprimerFichier = prompt("Nom du fichier à supprimer: ");
+        fs.unlink(supprimerFichier, function(error) { 
+            if (error) {
+                console.error("Erreur'", error); } 
+            else console.log("Fichier supprimé !"); 
+        });
+
+    } else if (response == "read"){
+        const readFichier = prompt("Nom du fichier: ");
+        fs.readFile(readFichier, 'utf8', function(error, data) {   
+            const content = data;   
+            console.log(content); });
     }
+    
+    else { console.log("Erreur") }
+};
 
-	process.exit();
-});
+f().then(operation);
